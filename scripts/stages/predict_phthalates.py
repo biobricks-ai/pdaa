1d688cd56789ac173b9786378e918098838de4e2
jpmikhail <johnpmikhail@gmail.com>
2025-08-28 02:51:54 -0400

    Intermediate edits

--- scripts/stages/predict_phthalates.py
+++ scripts/stages/predict_phthalates.py
@@ -6,9 +6,15 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from xgboost import XGBClassifier
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append('./')
+from scripts.utils.helpers import zscore_columns
 
 cachedir = Path("cache")
-modeldir = cachedir / "eadb"
+modeldir = cachedir / "combined"
 datadir = cachedir / "entity_similarity"
 
 # Create a new XGBClassifier instance
@@ -20,10 +26,17 @@ model.load_model(modeldir / "xgb_classifier_logRBA_model.json")
 activity_matrix_filled = pd.read_parquet(
     datadir / "activity_matrix_filled.parquet"
 )
+Z = zscore_columns(activity_matrix_filled)[0]
+MAV = Z.mean(axis=1)
+
+# Comparing predictions with assay clustering
+activity_by_cluster = pd.read_parquet("cache/assay_cluster_heatmap/activity_by_cluster.parquet")
+ZC = zscore_columns(activity_by_cluster)[0]
+CMAV = ZC.mean(axis=1)
 
 # scratch
 predictions = []
-for index, row in activity_matrix_filled.iterrows():
+for index, row in Z.iterrows():
     # # Get the InChI for the current row
     # inchi = row['inchi']
     
@@ -32,3 +45,27 @@ for index, row in activity_matrix_filled.iterrows():
     predictions.append(prediction)
 
 predictions = np.array(predictions)
+
+# Plotting
+plt.figure(figsize=(10, 6))
+sns.regplot(x=MAV, y=predictions, scatter_kws={'s': 10, 'color': 'red'}, line_kws={'color': 'red'})
+# plt.xlabel('Mean Activity Value (MAV)')
+plt.xlabel('MAV and CMAV')
+plt.ylabel('Binary log(RBA) Prediction')
+# plt.show()
+
+
+
+
+# # Plotting
+# plt.figure(figsize=(10, 6))
+sns.regplot(x=CMAV, y=predictions, scatter_kws={'s': 10, 'color': 'blue'}, line_kws={'color': 'blue'})
+# plt.xlabel('Clustered Mean Activity Value (CMAV)')
+# plt.ylabel('Binary log(RBA) Prediction')
+plt.show()
+
+plt.figure(figsize=(10, 6))
+sns.regplot(x=MAV, y=CMAV, scatter_kws={'s': 10, 'color': 'green'}, line_kws={'color': 'green'})
+plt.xlabel('MAV')
+plt.ylabel('CMAV')
+plt.show()
